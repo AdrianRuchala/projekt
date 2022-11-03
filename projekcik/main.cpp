22098bc7a129ad6776cf394b659fb55b80428f62
Adrian Ruchala <ar11389@student.pwsz-ns.edu.pl>
2022-11-03 12:41:19 +0100

    wer.1.0

---
+++ projekcik/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int x;
+    cout<<"Podaj a: ";
+    cin>>a;
+    cout<<"Podaj b: ";
+    cin>>b;
+    x=-b/a;
+    cout<<"Miejsce zerowe tej funkcji to: "<<x;;
+    return 0;
+}
