d6e8c8f08d60f8a179a65be7569198deaee45bf4
Adrian Ruchala <ar11389@student.pwsz-ns.edu.pl>
2022-11-03 12:55:19 +0100

    wer.1.2

--- projekcik/main.cpp
+++ projekcik/main.cpp
@@ -11,7 +11,17 @@ int main()
     cin>>a;
     cout<<"Podaj b: ";
     cin>>b;
+    if (a==0 && b==0)
+    {
+        x=0;
+        cout<<"Miejsce zerowe tej funkcji to: "<<x;
+    }else if (a==0 && b!=0)
+    {
+        cout<<"Funkcja nie ma miejsc zerowych";
+    }else
+    {
     x=-b/a;
-    cout<<"Miejsce zerowe tej funkcji to: "<<x;;
+    cout<<"Miejsce zerowe tej funkcji to: "<<x;
+    }
     return 0;
 }
