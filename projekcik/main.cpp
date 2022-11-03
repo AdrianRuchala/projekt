b2d8456d12e84918d09533a658e9d88979e2aa3b
Adrian Ruchala <ar11389@student.pwsz-ns.edu.pl>
2022-11-03 13:20:28 +0100

    wer.1.4

--- projekcik/main.cpp
+++ projekcik/main.cpp
@@ -6,22 +6,41 @@ int main()
 {
     int a;
     int b;
+    int c;
     int x;
-    cout<<"Podaj a: ";
-    cin>>a;
-    cout<<"Podaj b: ";
-    cin>>b;
-    if (a==0 && b==0)
+    int z;
+    cout<<"wybierz postac funkcji linowej: 1=postac kierunkowa, 2=postac ogolna"<<endl;
+    cin>>z;
+    switch (z)
     {
-        x=0;
+    case 1:
+        cout<<"Podaj a: ";
+        cin>>a;
+        cout<<"Podaj b: ";
+        cin>>b;
+        if (a==0 && b==0)
+        {
+            x=0;
+            cout<<"Miejsce zerowe tej funkcji to: "<<x;
+        }else if (a==0 && b!=0)
+        {
+            cout<<"Funkcja nie ma miejsc zerowych";
+        }else
+        {
+        x=-b/a;
         cout<<"Miejsce zerowe tej funkcji to: "<<x;
-    }else if (a==0 && b!=0)
-    {
-        cout<<"Funkcja nie ma miejsc zerowych";
-    }else
-    {
-    x=-b/a;
-    cout<<"Miejsce zerowe tej funkcji to: "<<x;
+        }
+        break;
+    case 2:
+        cout<<"Podaj A: ";
+        cin>>a;
+        cout<<"Podaj B: ";
+        cin>>b;
+        cout<<"Podaj C: ";
+        cin>>c;
+        cout<<"Miejsce zerowe tej funkcji to: "<<a;
+        break;
     }
+
     return 0;
 }
